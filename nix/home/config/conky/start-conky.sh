5775d4f11abe026dc7c725dcf7062f06f2353532
hz <dxp@ngs.ru>
2021-01-16 13:50:31 +0700

    conky: add system log

--- nix/home/config/conky/start-conky.sh
+++ nix/home/config/conky/start-conky.sh
@@ -5,4 +5,4 @@ killall conky
 conky -c ~/.config/conky/general
 conky -c ~/.config/conky/network
 conky -c ~/.config/conky/kern-log
-#conky -c ~/.config/conky/conkyrc3
+conky -c ~/.config/conky/system-log
