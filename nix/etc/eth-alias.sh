df412b116e2f3dc3c89f0a250d5463c699873375
hz <dxp@ngs.ru>
2021-01-16 14:31:07 +0700

    modify fake (virtual) eth0 interface creation script

--- nix/etc/eth-alias.sh
+++ nix/etc/eth-alias.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-modprobe dummy
-ifconfig dummy0 hw ether 00:26:18:9b:ab:3c
+modprobe dummy numdummies=1
+ip link add eth0 type dummy
+ifconfig eth0 hw ether 00:26:18:9b:ab:3c
+#ifconfig dummy0 hw ether 00:26:18:9b:ab:3c
 #ifup dummy0
-ip link set name eth0 dev dummy0
+#ip link set name eth0 dev dummy0
 
